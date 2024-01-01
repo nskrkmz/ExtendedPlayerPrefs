842835958fee1b9cd238496a8e93257a342bc1a3
nskrkmz <aeneskrkmz@gmail.com>
2024-01-01 21:05:14 +0300

    add char and Color operations

--- Nesco/ExtendedPlayerPrefs/ExtendedPlayerPrefs.cs
+++ Nesco/ExtendedPlayerPrefs/ExtendedPlayerPrefs.cs
@@ -11,6 +11,7 @@ namespace Nesco.EPP
     {
         private const string KEY_PATTERN = "^[a-zA-Z0-9]+$";
         private static Regex regex = new(KEY_PATTERN);
+        
         public static void DeleteKeys(string[] keys)
         {
             foreach (var key in keys)
@@ -41,9 +42,77 @@ namespace Nesco.EPP
                 SetString(key, value.ToString()[0].ToString());
         }
         #endregion
+
+        #region for char
+        public static char GetChar(string key)
+        {
+            if (IsKeyFormatValid(key) && HasKey(key))
+                return GetString(key)[0];
+            return ' ';
+        }
+        public static char GetChar(string key, char defaultValue)
+        {
+            if (IsKeyFormatValid(key) && HasKey(key))
+                return GetString(key)[0];
+            return defaultValue;
+        }
+        public static void SetChar(string key, char value)
+        {
+            if (IsKeyFormatValid(key))
+                SetString(key, value.ToString());
+        }
+        #endregion
+
+        #region for Color
+        public static Color GetColor(string key)
+        {
+            if (IsKeyFormatValid(key) && HasKey(key))
+                return StringToColor(GetString(key));
+            return Color.white;
+        }
+        public static Color GetColor(string key, Color defaultColor)
+        {
+            if (IsKeyFormatValid(key) && HasKey(key))
+                return StringToColor(GetString(key));
+            return defaultColor;
+        }
+        public static void SetColor(string key, Color color)
+        {
+            if (IsKeyFormatValid(key))
+            {
+                SetString(key, ColorToString(color));
+            }
+        }
+        #endregion
         private static bool IsKeyFormatValid(string key)
         {
             return regex.IsMatch(key);
+        }
+
+        private static Color StringToColor(string colorCode)
+        {
+            Color color = Color.clear;
+            string[] components = colorCode.Split(',');
+
+            if (components.Length == 4)
+            {
+                bool state = float.TryParse(components[0], out color.r) &&
+                float.TryParse(components[1], out color.g) &&
+                float.TryParse(components[2], out color.b) &&
+                float.TryParse(components[3], out color.a);
+
+                if(!state)
+                    throw new FormatException();
+            }
+            else
+                throw new FormatException();
+
+            return color;
+        }
+
+        private static string ColorToString(Color color)
+        {
+            return $"{color.r},{color.g},{color.b},{color.a}";
         }
     }
 }
