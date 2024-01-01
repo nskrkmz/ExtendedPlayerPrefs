2a67f53346fc6bf64ec3ba9402d73ab2dae2903a
nskrkmz <aeneskrkmz@gmail.com>
2024-01-01 19:15:24 +0300

    init

---
+++ Nesco/ExtendedPlayerPrefs/ExtendedPlayerPrefs.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Nesco.EPP
+{
+    public class ExtendedPlayerPrefs : PlayerPrefs
+    {
+       
+    }
+}
+
---
+++ Nesco/ExtendedPlayerPrefs/Test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Nesco.EPP;
+public class Test : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
