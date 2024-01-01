83811904612609b12123e1f97ba03d22818654e6
nskrkmz <aeneskrkmz@gmail.com>
2024-01-01 19:22:55 +0300

    Validate key

--- Nesco/ExtendedPlayerPrefs/ExtendedPlayerPrefs.cs
+++ Nesco/ExtendedPlayerPrefs/ExtendedPlayerPrefs.cs
@@ -8,7 +8,13 @@ namespace Nesco.EPP
 {
     public class ExtendedPlayerPrefs : PlayerPrefs
     {
-       
+        private static bool IsKeyFormatValid(string key)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+                throw new ArgumentNullException(nameof(key), $"argument {nameof(key)} cannot be null or white space.");
+
+            return true;
+        }
     }
 }
 
